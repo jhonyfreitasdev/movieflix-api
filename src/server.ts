d7d5b275f5e00c62956b2201934297e888069591
Jhony Freitas <jhony00._@hotmail.com>
2024-06-03 21:06:43 -0300

    Usando PrismaClient para retornar os filmes na rota

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
 const port = 3000;
 const app = express();
 
-app.get('/movies', (req, res) => {
-  res.send('Listagem de filmes');
+app.get('/movies', async (req, res) => {
+  const movies = await prisma.movie.findMany();
+  res.json(movies); 
 });
 
-// teste
 app.listen(port, () => {
   console.log(`Server running on port http://localhost:${port}`);
 });
