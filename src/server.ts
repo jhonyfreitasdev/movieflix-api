e5c9609758f3e4cf8f5015763f42358e6bf532c1
Jhony Freitas <jhony00._@hotmail.com>
2024-06-05 20:06:19 -0300

    Ordenando filmes por ordem crescente e adicionando gênero e linguagem

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,16 @@ const prisma = new PrismaClient();
 const port = 3000;
 const app = express();
 
-app.get('/movies', async (req, res) => {
-  const movies = await prisma.movie.findMany();
+app.get('/movies', async (_req, res) => {
+  const movies = await prisma.movie.findMany({
+    orderBy: { 
+      title: "asc" 
+    },
+    include: {
+      languages: true,
+      genres: true,
+    }
+  });
   res.json(movies); 
 });
 
